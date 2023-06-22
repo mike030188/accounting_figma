e8f15beba00750f5dbc2ee985ca97f6e605a75b3
mike030188 <halol.imnida2023@gmail.com>
2023-06-22 07:38:54 +0500

    feat: Creating SideBar

---
+++ src/components/sidebar/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div style={{backgroundColor: '#1A202E', width: '275px', height: '100vh', color: 'white'}}>
+        <h1>Accounting</h1>
+        <div>Dashboard</div>
+        <div>Accounting</div>
+        <div>Transactions</div>
+        <div>Reports</div>
+        <div>Agency</div>
+        <div>Settings</div>
+        <div>Trash</div>
+    </div>
+  )
+}
+
+export default SideBar
