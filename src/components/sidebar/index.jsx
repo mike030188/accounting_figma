7592984367f76cb2affd42c1e1a7dcecce21e461
mike030188 <halol.imnida2023@gmail.com>
2023-06-22 15:55:36 +0500

    feat: revising SideBar and installing reactRouterDom

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const SideBar = () => {
   return (
-    <div style={{backgroundColor: '#1A202E', width: '275px', height: '100vh', color: 'white'}}>
-        <h1>Accounting</h1>
-        <div>Dashboard</div>
-        <div>Accounting</div>
-        <div>Transactions</div>
-        <div>Reports</div>
-        <div>Agency</div>
-        <div>Settings</div>
-        <div>Trash</div>
+    <div style={{backgroundColor: '#1A202E', width: '275px', height: '100vh', padding: '25px 21px'}}>
+        <h1 style={{color: 'white'}}>Accounting</h1>
+        <div style={{color: '#CFD8E3', fontSize: '14px', cursor: 'pointer', height: '43px', marginTop: '48px'}}>Dashboard</div>
+        <div style={{color: '#CFD8E3', fontSize: '14px', cursor: 'pointer', height: '43px',}}>Accounting</div>
+        <div style={{color: '#CFD8E3', fontSize: '14px', cursor: 'pointer', height: '43px',}}>Transactions</div>
+        <div style={{color: '#CFD8E3', fontSize: '14px', cursor: 'pointer', height: '43px',}}>Reports</div>
+        <div style={{color: '#CFD8E3', fontSize: '14px', cursor: 'pointer', height: '43px',}}>Agency</div>
+        <div style={{color: '#CFD8E3', fontSize: '14px', cursor: 'pointer', height: '43px',}}>Settings</div>
+        <div style={{color: '#CFD8E3', fontSize: '14px', cursor: 'pointer', height: '43px',}}>Trash</div>
     </div>
   )
 }
